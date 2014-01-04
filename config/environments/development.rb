7e909c80c811104ca68837ff42105ade38d50cc5
Rustem <rustem_ss@mail.ru>
2014-01-04 16:39:16 +0200

    Implemented authorization through Device

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,13 @@ Cook4me::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Ensure you have defined default url options in your environments files. Here
+  # is an example of default_url_options appropriate for a development environment
+  # in config/environments/development.rb:
+
+  #      config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
